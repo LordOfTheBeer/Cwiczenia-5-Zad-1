4a292bd2ec4a8a7f86ad2ea6e8ab0a8b2b4054d3
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-14 13:56:52 +0100

    Add files via upload

---
+++ Cwiczenia 5 Zad 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Cwiczenia_5_Zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Podaj rozmiar tablicy");
+            n = System.Int16.Parse(Console.ReadLine());
+
+            int[] tab1 = new int[n];
+
+            int j;
+            for (j = 0; j < tab1.Length; j++)
+            {
+                Console.WriteLine("Podaj Liczbe");
+                tab1[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (j = 0; j < tab1.Length; j++)
+            {
+                //Console.Write(tab1[j] + "\t");
+            }
+            Console.WriteLine(tab1[tab1.Length-2]);
+        }
+    }
+}
